abce6cce9b1e0fe8f6f9c58f6dca7442b40d5a56
Prabhat <prabhat.nagarajan@gmail.com>
2017-03-26 04:16:12 -0500

    adds model estimator

---
+++ model_estimator.py
@@ -0,0 +1,39 @@
+import numpy as np
+from mdp import *
+
+class ModelEstimator:
+
+	def __init__(self, num_states, num_actions):
+		self.rewards = np.zeros(num_states)
+		self.reward_counts = np.zeros(num_states)
+		self.transitions = np.zeros((num_states, num_actions, num_states))
+		self.transition_counts = np.zeros((num_states, num_actions, num_states))
+
+	def add_transition(self, state, action, next_state):
+		self.transition_counts[state, action, next_state] = self.transition_counts[state, action, next_state] + 1
+
+	def add_reward(self, state, reward):
+		count = self.reward_counts[state]
+		self.rewards[state] = self.rewards[state] * (count/(count +_ 1)) + (1/(count + 1)) * reward
+		self.reward_counts[state] = count + 1
+
+	def set_transition_distribution(self):
+		for state in range(np.shape(self.transition_counts)[0]):
+			for action in range(np.shape(self.transition_counts)[1]):
+				total = np.sum(self.transition_counts[state, action,:])
+				if total == 0:
+					for next_state in range(np.shape(self.transitions_counts)[2]):
+						#If no data assume uniform transitions
+						self.transitions[state, action, next_state] = 1/np.shape(self.transitions_counts)[2]
+				else:
+					for next_state in range(np.shape(self.transitions_counts)[2]):
+						self.transitions[state, action, next_state] = self.transition_counts[state, action, next_state]/total
+
+	def get_model(self):
+		self.set_transition_distribution()
+		return (self.transitions, self.rewards)
+
+	def get_as_mdp(self, gamma, start=None, terminal=None, max_value=1000):
+		self.set_transition_distribution()
+		#Need to check transition dynamics
+		return MDP(self.transitions, self.rewards, gamma, start, terminal, max_value)
